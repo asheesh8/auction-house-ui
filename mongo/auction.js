af0e2d1669d970514cfad45a723e6b50a1ecfaa9
Loganest2110 <logan.donoghue@mymail.champlain.edu>
2026-04-13 14:04:51 -0400

    Added auction function for Mongo, may need to be changed

--- mongo/auction.js
+++ mongo/auction.js
@@ -1,1 +1,6 @@
-// To do: Add function for displaying active auctions by end time
+import { auctionsExport, bidsExport } from "./schema";
+
+async function get_active_auctions() {
+    const activeAuctions = (await auctionsExport.find({active: true, }).limit(10)).sort({end_date: 1});
+    return activeAuctions;
+}
