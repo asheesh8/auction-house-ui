7f893607dbc6c0c659ca8bc93fa65c3908a4d85b
CDiBartolo05 <calvindoe76@gmail.com>
2026-04-14 19:31:40 -0400

    added return types for write functions
    turns out I was overthinking it

--- postgres/auctions.js
+++ postgres/auctions.js
@@ -1,28 +1,18 @@
 import client from "./client.js";
 // will return all auctions in progress
 // not all info, just perfunctory info for quick display
-//is the async actually necessary here? all it seems to do is make impossible to do anything after the await
 //TODO: not finished, commented out for now so it doesn't mess with other functions
 async function get_active_auctions() {
-    const result = await client.query(
-        "SELECT item_name, end_date FROM auctions WHERE status = 'In-Progress'",
-        function (err, data, fields) {
-            if (err) throw err;
-            console.log(data.rows); //successfully prints out rows. TODO: set up actual return array
-        }
-    );
+    const result = await client.query("SELECT item_name, end_date FROM auctions WHERE status = 'In-Progress'");
+    //console.log(result);
+    return result.rows;
 }
 
 // get more detailed info a specific auction
 async function get_auction_details(auction_item) {
-    const result = await client.query(
-        "SELECT item_name, end_date, description FROM auctions WHERE item_name = $1",
-        [auction_item],
-        function (err, data, fields) {
-            if (err) throw err;
-            console.log(data.rows); //succesfully prints out rows. TODO: set up actual return
-        }
-    )
+    const result = await client.query("SELECT item_name, end_date, description FROM auctions WHERE item_name = $1", [auction_item]);
+    //console.log(result);
+    return result.rows;
 }
 
 // will write a new auction to the postgres database
@@ -32,8 +22,11 @@ async function write_auction(seller, item, desc) {
     const result = client.query(
         "INSERT INTO auctions (seller, item_name, description) VALUES ($1, $2, $3)",
         [seller, item, desc]
-    )
+    );
     return result.rowCount > 0
 }
 
+//Note: these functions have to be awaited to work, like below
+// const auctions = await get_active_auctions();
+// console.log(auctions);
 //TODO: functions for updating auction state and other administrative tasks
--- postgres/bids.js
+++ postgres/bids.js
@@ -1,14 +1,10 @@
 import client from "./client.js";
 // gets the top bid of an auction
+// should this also return the account, or should that be kept anonymous?
 async function get_top_bid(auction_item) {
-    const result = await client.query(
-        "SELECT amount FROM bids b JOIN auctions a ON a.id = b.auction_id WHERE a.item_name = $1 AND b.top_bid = true;",
-        [auction_item],
-        function (err, data, fields) {
-            if (err) throw err;
-            console.log(data.rows);
-        }
-    )
+    const result = await client.query("SELECT amount FROM bids b JOIN auctions a ON a.id = b.auction_id WHERE a.item_name = $1 AND b.top_bid = true;", [auction_item]);
+    console.log(result.rows);
+    return result.rows;
 }
 
 // updates the top bid of an auction
@@ -18,8 +14,10 @@ function set_top_bid() {
 
 // place a new bid on an auction.
 // if it is higher than the current highest bid, set as the new highest bid
-function new_bid() {
-
+function new_bid(auction_item, amount) {
+    // const result = client.query(
+    //
+    // )
 }
 
 //get_top_bid("House of the Dead Original Arcade Machine");
